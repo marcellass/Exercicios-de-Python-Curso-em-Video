cdc1609a7afc59b67615d78c20c3611ee5634b5c
marcellass <marcella.ssanchesbe@gmail.com>
2021-06-07 09:44:56 -0300

    Update ex035.py

--- exercicios/ex035.py
+++ exercicios/ex035.py
@@ -10,4 +10,4 @@ print('-=' * 20)
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM formar triângulo')
 else:
-    print('Os segmentos acima NÃO podem formar um triângulo')
+    print('Os segmentos acima NÃO podem formar um dos triângulo')
